eb59b54b5079fce99fad0f3181b89842e5f13025
Bhupendra <keycracker7@gmail.com>
2021-06-16 15:29:23 +0530

    adding project

---
+++ org_files/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div 
+    <div>
+      <img alt='robots'src='https://robohash.org/test'/>
+      <div>
+      <h2>Bhupendra sahu</h2>
+      <p>Bhupendra@gmail.com</p>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import Card from './Card';
+import App from './App';
+
+
+import reportWebVitals from './reportWebVitals';
+//import 'tachyons';
+//import {robots} from './robots';
+
+ReactDOM.render(
+	<App/>
+	, document.getElementById('root')
+);
+reportWebVitals();
